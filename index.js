7ba90a7f1c114f70a0fd6fe8848ff6e76c0f929a
Damian <swean@vp.pl>
2023-04-05 19:28:45 +0200

    debuging1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Movie  from './class.js'
 
 const inp = document.querySelector('input')
 const searchBtn = document.getElementById('search-btn')
+const bgIcon = document.querySelector(".background")
 let searchHistory = []
 let film = ''
 let searchedFilm = {}
@@ -15,14 +16,20 @@ searchBtn.addEventListener('click', () => {    //------------------------getting
 fetch (`http://www.omdbapi.com/?t=${inp.value}&page=2&apikey=f882b954&`)
 .then (res => res.json())
 .then (data => searchResult(data.Title,data.Year,data.imdbRating,data.Runtime,data.Genre,data.Plot,data.Poster))
+.catch (noResult())
+bgIcon.classList.add('hide')
 })
+
+function noResult(){
+    film = ''
+}
 
 function searchResult(title,year,rate,min,genre,describsion,poster){ 
 
 film = `<section><img class="poster" src="${poster}" alt="poster ${title}"><div class="spec">
-<div class="top"><h3>${title}</h3><h4>⭐ ${rate} </h4><h4> ${year}</h4></div>
-<div class="bottom"><h4>${min}  ${genre} </h4> <div class="add-section"><button type="button" id="${title}Btn" class="add" data-add="${title}">❌</button></div> </div></div> 
-<article class="describsion">${describsion}</article></section>`
+<div class="top"><h3>${title}</h3><h5>⭐ ${rate} </h5><h5> ${year}</h5></div>
+<div class="bottom"><h5>${min}</h5><h5>  ${genre} </h5> <div class="add-section"><h5>Watchlist</h5><button type="button" id="${title}Btn" class="add" data-add="${title}">❌</button></div> </div></div> 
+<article class="describsion">${describsion}<hr></article></section>`
 //-------------------Store data in string to display it as html
 
 searchedFilm = {title: title, // --------------- object witch searched title and string(html) data
@@ -40,9 +47,11 @@ document.addEventListener('click', (e)=>{  //-------------------- click watchlis
 
     if ( e.target.attributes[0].value === "button"){
         if (searchedFilm.title !== undefined){
-             watchList.unshift(searchedFilm.title)
+            searchHistory.filter(title => title ==`<h3>${e.target.dataset.add}</h3>`)
+            console.log(searchHistory)
+             watchList.unshift(e.target.dataset.add)
        localStorage.setItem( "watchlist",`${JSON.stringify(watchList)}`) //  -------------  if event is equal type "button" ads this title to watchlist and saves in local storage
-       let store = new Movie(searchedFilm)
+       let store = new Movie(searchHistory[0])
     store.save()} // ----------------  saves string (html) into local storage using class property function exported from class.js
 }})
 
